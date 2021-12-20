2f77bd77f587c44940fddb003a5abc7a85528a44
1074597891 <1074597891@qq.com>
2021-12-20 21:19:37 +0800

    改

--- open_txt.py
+++ open_txt.py
@@ -18,13 +18,16 @@ def trip():
         (str(list_ypd[nums]) == "['挺哥无敌']") and str(list_ypd[nums + 1]) == "[队列中']") == 1):
             web.open('https://api.day.app/QFBnuUtQaSuxZns6Lz88eS/姚培栋上线了')
             tkinter.messagebox.showinfo('提示', '姚培栋上线了')
+            time.sleep(5)
+            os.system('taskkill /F /IM chrome.exe')
 
         elif (((str(list_ypd[nums]) == "['挺哥无敌、']") and str(list_ypd[nums + 1]) == "['在线']") or (
         (str(list_ypd[nums]) == "['挺哥无敌、']") and str(list_ypd[nums + 1]) == "['游戏中']") or (
         (str(list_ypd[nums]) == "['挺哥无敌、']") and str(list_ypd[nums + 1]) == "[队列中']") == 1):
             web.open('https://api.day.app/QFBnuUtQaSuxZns6Lz88eS/姚培栋上线了')
             tkinter.messagebox.showinfo('提示', '姚培栋上线了')
-
+            time.sleep(5)
+            os.system('taskkill /F /IM chrome.exe')
 
         names_qm = lines[:-1].split('\n')
         list_qm.append(names_ypd)
@@ -34,7 +37,8 @@ def trip():
             (str(list_qm[nums1]) ==  "['骑着肖亮亮逛世界']"  ) and str(names_qm[nums1 + 1]) ==  "[队列中']"  ) == 1) :
                 web.open('https://api.day.app/QFBnuUtQaSuxZns6Lz88eS/乔梦上线了')
                 tkinter.messagebox.showinfo('提示', '乔梦上线了')
-
+                time.sleep(5)
+                os.system('taskkill /F /IM chrome.exe')
 
 if __name__ == '__main__':
     trip()
