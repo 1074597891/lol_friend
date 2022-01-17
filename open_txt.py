17909180e0dfa85d1fbeefe3be71056808db094f
1074597891 <1074597891@qq.com>
2022-01-17 11:38:24 +0800

    修改

--- open_txt.py
+++ open_txt.py
@@ -11,6 +11,7 @@ def trip():
     f_ypd = open("1.txt", "r")
     for lines in f_ypd.readlines():
         names_ypd = lines[:-1].split('\n')
+        print(names_ypd)
         list_ypd.append(names_ypd)
     for nums in range(0, len(list_ypd)):
         if (((str(list_ypd[nums]) == "['挺哥无敌']") and str(list_ypd[nums + 1]) == "['在线']") or (
@@ -20,7 +21,7 @@ def trip():
             tkinter.messagebox.showinfo('提示', '姚培栋上线了')
             os.mkdir("./log_in")
             time.sleep(5)
-            os.system('taskkill /F /IM chrome.exe')
+            os.system('taskkill /F /IM msedge.exe')
 
 
         elif (((str(list_ypd[nums]) == "['挺哥无敌、']") and str(list_ypd[nums + 1]) == "['在线']") or (
@@ -30,7 +31,7 @@ def trip():
             tkinter.messagebox.showinfo('提示', '姚培栋上线了')
             os.mkdir("./log_in")
             time.sleep(5)
-            os.system('taskkill /F /IM chrome.exe')
+            os.system('taskkill /F /IM msedge.exe')
     f_ypd.close()
 
     f_qm = open("1.txt", "r")
@@ -45,14 +46,14 @@ def trip():
             tkinter.messagebox.showinfo('提示', '乔梦上线了')
             os.mkdir("./log_in")
             time.sleep(3)
-            os.system('taskkill /F /IM chrome.exe')
+            os.system('taskkill /F /IM msedge.exe')
         if(((str(list_qm[nums1]) == "['玩死你的是你爹']") and str(list_qm[nums1 + 1]) == "['在线']") or (
                 (str(list_qm[nums1]) == "['玩死你的是你爹']") and str(list_qm[nums1 + 1]) == "['游戏中']") or (
                 (str(list_qm[nums1]) == "['玩死你的是你爹']") and str(list_qm[nums1 + 1]) == "[队列中']") == 1):
             tkinter.messagebox.showinfo('提示', '小号上线了')
             os.mkdir("./log_in")
             time.sleep(3)
-            os.system('taskkill /F /IM chrome.exe')
+            os.system('taskkill /F /IM msedge.exe')
     f_qm.close()
     time.sleep(5)
 
